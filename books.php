adeeeaa799b4d7e319a3c0cbe6595c53a3e6277f
saber315127 <37133549+saber315127@users.noreply.github.com>
2019-06-03 18:30:28 +0800

    Update books.php

--- books.php
+++ books.php
@@ -200,3 +200,66 @@ if($_GET['name'])
 	$sql="";
 }
 ?>
+<br>
+=======================================================================================================================================
+<br>
+<body><form name="forml" method="get" action="books.php">
+輸入查詢書本編號:
+<?
+$sql="select * from books";
+$result=mysql_query($sql);
+?>
+<select name="bookno2" size="1">
+<?
+while ($row=mysql_fetch_array($result))
+{
+	echo "<option value=$row[0]>$row[0] ";
+}
+?>
+</option>
+</select>
+<br>
+<input type="submit" value='查詢'>
+<input type= "reset" value='重設'>
+</form>
+</body>
+</html>
+<br>
+<?
+if ($_GET['bookno2'])
+{
+	$bookno2=$_GET['bookno2'];
+	$sql="select *from books where books.書籍編號 = $bookno2";
+	$result=mysql_query($sql);
+	$row=mysql_fetch_array($result);
+}
+?>
+<body><form name="form1" method="get" section="books.php">
+輸入書本名稱:
+<input type="text" name="name3" value="<? echo $row[1]; ?>" maxlength="20" size="20"><br>
+輸入書本價格:
+<input type="text" name="cost3" value="<? echo $row[2]; ?>" maxlength="6" size="20"><br>
+負責員工編號:
+<input type="text" name="emp3" value="<? echo $row[3]; ?>" maxlength="6" size="20"><br>
+
+<input type="hidden" name="no3" value="<? echo $row[0]; ?>" maxlength="6" size="20"><br>
+<input type="submit" value='修改'>
+<input type="reset" value='重設'>
+</form>
+</body>
+</html>
+<?
+if($_GET['name3'])
+{
+	$bookno3=$_GET['no3'];
+	$bookname3=$_GET['name3'];
+	$bookcost3=$_GET['cost3'];
+	$bookemp3=$_GET['emp3'];
+	
+	$sql="update books set 書籍名稱='$bookname3', 價格=$bookcost3, 負責員工編號= $bookemp3 where 書籍編號= $bookno3 ";
+	$result=mysql_query($sql);
+	
+	echo "<br>";
+	echo "你已經修改書本編號 $bookno3" ;
+}
+?>
